cc86cad5e4b10fadf237fcb126cb72bea45bf3a9
dylcruz <dylan@dylancruz.me>
2025-09-01 14:43:47 -0700

    add front end

--- cookies-based-shopping-cart/server.py
+++ cookies-based-shopping-cart/server.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, g, make_response, request
+from flask import Flask, g, request, render_template
 
 app = Flask(__name__)
 
@@ -13,12 +13,31 @@ products = [
 
 @app.before_request
 def parse_cart():
-    cart_cookie_value = request.cookies.get("cart")
+    cart_cookie_value = request.cookies.get("cart") or '[]'
     cart_ids = json.loads(cart_cookie_value)
     g.cart_ids = cart_ids
 
 
-@app.get("/cart")
+@app.get('/products')
+def products_page():
+    products_with_added_data = [{**product, 'isInCart': product.get('id') in g.cart_ids}  for product in products]
+    return render_template('products.html', products=products_with_added_data)
+
+
+@app.get('/cart')
+def cart_page():
+    cart_items = [
+        next(product for product in products if product["id"] == id)
+        for id in g.cart_ids
+    ]
+    return render_template(
+        'shopping-cart.html', 
+        products=cart_items, 
+        cartIsEmpty=len(cart_items) == 0
+        )
+
+
+@app.get("/api/cart")
 def load_user_cart():
     cart_items = [
         next(product for product in products if product["id"] == id)
@@ -27,21 +46,21 @@ def load_user_cart():
     return cart_items
 
 
-@app.put("/cart")
+@app.put("/api/cart")
 def add_to_cart():
     item_id_to_add = request.json["id"]
     g.cart_ids.append(item_id_to_add)
-    return "Successfully added item to cart!"
+    return {"message": "Successfully added item to cart!" }
 
 
-@app.delete("/cart/<string:item_id>")
+@app.delete("/api/cart/<string:item_id>")
 def delete_from_cart(item_id):
     remove_item_id = next(id for id in g.cart_ids if id == item_id)
     g.cart_ids.remove(remove_item_id)
     return "Successfuly removed item from cart!"
 
 
-@app.delete("/cart/clear")
+@app.delete("/api/cart/clear")
 def clear_cart():
     g.cart_ids = []
     return "Successfully cleared the cart!"
